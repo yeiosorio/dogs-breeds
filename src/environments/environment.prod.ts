7d405ae2fb6788b9a568f705c5e69566306938c2
yeiosorio <yei_osorio@hotmail.com>
2025-04-08 23:04:47 -0500

    Init: (initial config): project created

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://api.dogsbreeds.com/api',
+  version: '1.0.0',
+  appName: 'Dogs Breeds App'
+}; 
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:4200/api',
+  version: '1.0.0',
+  appName: 'Dogs Breeds App'
+}; 
